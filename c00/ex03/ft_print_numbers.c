d5f061bed9416b4bab720389e508adf23c142ca4
Mun Chung Chang <munchang@F2N014.42adel.org.au>
2025-01-25 13:25:32 +1030

    ex03 added to master branch

---
+++ c00/ex03/ft_print_numbers.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: munchang <munchang@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 11:30:48 by munchang          #+#    #+#             */
+/*   Updated: 2025/01/24 11:48:15 by munchang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_numbers(void)
+{
+	char num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		ft_putchar(num);
+		num++;
+	}
+}
+/*
+int main(void)
+{
+	ft_print_numbers();
+	return(0);
+}
+*/

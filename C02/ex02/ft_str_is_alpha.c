7068f140473c3a79f89f290bd5fba0218b0b874b
Mun Chung Chang <munchang@F2N085.42adel.org.au>
2025-02-05 16:21:12 +1030

    send ex00 ex01 ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,8 +6,34 @@
 /*   By: munchang <munchang@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 19:29:32 by munchang          #+#    #+#             */
-/*   Updated: 2025/02/04 19:29:52 by munchang         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:16:38 by munchang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int	output;
+	char	*message;
+
+	message = "Hello";
+	output = ft_str_is_alpha(message);
+	printf("%d\n", output);
+}
